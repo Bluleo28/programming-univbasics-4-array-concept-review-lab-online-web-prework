831487d3359b4419b77874ba1228930222ebad57
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-24 02:19:50 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
-def find_element_index(array, value_to_find)
-  # Add your solution here
+find_element_index(array, value_to_find)
+(find_element_index(scale,2)).to eq(0)
 end
 
 def find_max_value(array)
-  # Add your solution here
+(find_max_value(scale)) .to eq(5)
 end
 
 def find_min_value(array)
-  # Add your solution here
+(find_min_value(scale)) .to eq(2)
 end
